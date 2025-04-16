3d8a4128f8627a4bf7f895f39b66c2a5bda129bb
Shaoting <shaotingf@uchicago.edu>
2025-04-16 01:06:37 -0500

    change vllm to v0 version
    Signed-off-by: Shaoting <shaotingf@uchicago.edu>

--- bootstrapper.py
+++ bootstrapper.py
@@ -229,6 +229,7 @@ class LocalVllmBootstrapper(Bootstrapper):
             print(f"\033[32mLaunching Engine with Command :\033[0m LMCACHE_CONFIG_FILE={self.config.lmcache_config.config_path} vllm serve {self.config.vllm_config.cmdargs()} {self.config.vllm_optional_config.cmdargs()} {extra_args} --kv-transfer-config \'{ktc}\'")
             return f"vllm serve {self.config.vllm_config.cmdargs()} {self.config.vllm_optional_config.cmdargs()} {extra_args} --kv-transfer-config \'{ktc}\'"
         else:
+            os.environ["VLLM_USE_V1"]="0"
             print(f"\033[32mLaunching Engine with Command :\033[0m vllm serve {self.config.vllm_config.cmdargs()} {self.config.vllm_optional_config.cmdargs()} {extra_args}")
             return f"vllm serve {self.config.vllm_config.cmdargs()} {self.config.vllm_optional_config.cmdargs()} {extra_args}"
 
